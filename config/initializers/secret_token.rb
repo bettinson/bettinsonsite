81ffe434f9d907e65bb9a871c1233a87cab496e2
Matt Bettinson <mattbettinson@me.com>
2012-10-10 16:21:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bettinsonsite::Application.config.secret_token = '2518d9e8c40664f235ec0dfc27bd0335c26b01fb60211bab67ff430eea7f7fe7f9398fe8b410e8a0f42dc8f95f2b23a66904a0ae1d31ee6a037ef1440a533ecc'
