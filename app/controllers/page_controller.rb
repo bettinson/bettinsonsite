cdc6cdac79147c8ff02e7baf4d1b7883bc7cdf17
Matt Bettinson <mattbettinson@me.com>
2012-10-28 13:46:21 -0400

    New, cleaner home screen

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,7 @@
 class PageController < ApplicationController
   def start_page
     @github = "http://www.github.com/bettinson"
-    @twitter = "http://www.twitter.com/mattbettinson"
+    @twitter = "https://twitter.com/bttnsn"
     @dribbble = "http://www.dribbble.com/bettinson"
     @soundcloud = "http://www.soundcloud.com/aethrum"
     @email = "mailto:mattbettinson@me.com"
