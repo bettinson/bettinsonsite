fb5c064c1ede92cc9de04d3b3d213d1c6914e77c
Matt Bettinson <mattbettinson@Matthews-MacBook-Pro.local>
2012-11-24 14:09:30 -0500

    Changed twitter URL

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,7 @@
 class PageController < ApplicationController
   def start_page
     @github = "http://www.github.com/bettinson"
-    @twitter = "https://twitter.com/bttnsn"
+    @twitter = "https://twitter.com/bettinsn"
     @dribbble = "http://www.dribbble.com/bettinson"
     @soundcloud = "http://www.soundcloud.com/aethrum"
     @email = "mailto:mattbettinson@me.com"
