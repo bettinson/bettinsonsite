64c0d46cb453732962c147566549ab7e5c78a5ff
Matt Bettinson <mattbettinson@me.com>
2013-04-28 20:53:57 -0400

    Changed some links

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,9 @@
 class PageController < ApplicationController
   def start_page
     @github = "http://www.github.com/bettinson"
-    @twitter = "https://twitter.com/bettinsn"
+    @twitter = "https://twitter.com/mattbettinson"
     @dribbble = "http://www.dribbble.com/bettinson"
-    @soundcloud = "http://www.soundcloud.com/aethrum"
+    @soundcloud = "http://www.soundcloud.com/mattbettinson"
     @email = "mailto:mattbettinson@me.com"
   end
 end
